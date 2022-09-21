a48ae3094de8bf21e282ce5479a99662d1d53436
Roman <i@zangetsu.ru>
2022-09-21 19:30:21 +0500

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void Task64()
+{
+    Console.WriteLine("Введите число M: ");
+    int numberM = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число N: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(IntervalNaturalNumbers(numberM, numberN));
+}
+
+int IntervalNaturalNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }  
+    else if (m > n)
+    {
+        Console.Write($"{IntervalNaturalNumbers(m, n + 1)}, ");
+        return n;
+    }
+    else
+        Console.Write($"{IntervalNaturalNumbers(m, n - 1)}, ");
+    return n;
+}
+
+Task64();
---
+++ Task_64v2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void Task64()
+{
+    Console.WriteLine("Введите число M: ");
+    int numberM = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число N: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+
+    IntervalNaturalNumbers(numberM, numberN);
+}
+
+void IntervalNaturalNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        while (m <= n)
+        {
+            Console.Write($"{m} ");
+            m += 1;
+        }
+    }
+    else
+    {
+        while (n <= m)
+        {
+            Console.Write($"{m} ");
+            m -= 1;
+        }
+    }
+}
+
+Task64();
