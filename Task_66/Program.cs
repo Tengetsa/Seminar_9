05280916c1e151e7ee6815a8bd73b07c311274a6
Roman <i@zangetsu.ru>
2022-09-21 19:32:14 +0500

    Task_66

---
+++ Task_66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void Task66()
+{
+    Console.WriteLine("Введите число M: ");
+    int numberM = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число N: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine();
+    Console.WriteLine($"{TheSumOfNaturalElements(numberM, numberN)}");
+}
+
+int TheSumOfNaturalElements(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    else if (m > n)
+    {
+        return m + TheSumOfNaturalElements(m - 1, n);
+    }
+    else
+    {
+        return m + TheSumOfNaturalElements(m + 1, n);
+    }
+}
+
+Task66();
---
+++ Task_66v2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void Task66()
+{
+    Console.WriteLine("Введите число M: ");
+    int numberM = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число N: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine();
+    Console.WriteLine($"Сумму натуральных элементов в промежутке от {numberM} до {numberN} = {TheSumOfNaturalElements(numberM, numberN)}");
+}
+
+int TheSumOfNaturalElements(int m, int n)
+{
+    int result = m;
+    if (m < n)
+    {
+        while (m < n)
+        {
+            m += 1;
+            result += m;
+        }
+        return result;
+    }
+    else
+    {
+        while (n < m)
+        {
+            m -= 1;
+            result += m;
+        }
+        return result;
+    }
+}
+
+Task66();
